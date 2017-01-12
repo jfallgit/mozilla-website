a51cc05e29e58a3dd71ac55e455cce03d760f2ae
James Fallon <jamesstephenfallon@gmail.com>
2017-01-12 15:06:51 -0500

    add fake virus messages and click reactions

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-document.querySelector('img').onclick = function() {
-	alert('Please dont poke the image!');
+document.querySelector('img').onclick = function () {
+	alert('Please dont poke doge!');
 }
 
-document.querySelector('#dogQuote').onclick = function() {
+document.querySelector('#dogQuote').onclick = function () {
 	alert('Dog quote superior to other quotes');
+}
+
+document.querySelector('#falsedogQuote').onclick = function () {
+	alert('This dog quote not authentic');
 }
 
 alert('CAUTION: VIRUS DETECTED')
