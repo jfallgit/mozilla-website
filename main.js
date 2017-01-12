d916b5c45193cdaae6b7c900e50e402b9d53baa1
James Fallon <jamesstephenfallon@gmail.com>
2017-01-12 14:52:00 -0500

    add javascript to website

---
+++ main.js
@@ -0,0 +1,18 @@
+//javascript
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('img').onclick = function() {
+	alert('Please dont poke the image!');
+}
+
+document.querySelector('#dogQuote').onclick = function() {
+	alert('Dog quote superior to other quotes');
+}
+
+alert('CAUTION: VIRUS DETECTED')
+alert('💀YOUR COMPUTER HAS BEEN INFECTED💀')
+alert('Dogs deployed to fight virus 💀💥🐕🐕🐕🐕🐕🐕🐕')
+alert('Virus defeated')
+
